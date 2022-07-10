1e9ab779c06b608d4390e87fc634c14bc691d738
DhananjayK97 <109036429+DhananjayK97@users.noreply.github.com>
2022-07-10 23:05:30 +0530

    Update Shape.java

--- src/Shape.java
+++ src/Shape.java
@@ -1,4 +1,20 @@
+class Shapes {
+    String color;
 
-public class Shape {
+    Shapes(String color){
+        this.color = color;
+    }
 
+    double calculateArea(){
+        return 0;
+    }
+
+    double calculatePerimeter(){
+        return 0;
+    }
+
+    void displayColor(){
+        System.out.println("the collor is " + "" + this.color);
+        return;
+    }
 }
